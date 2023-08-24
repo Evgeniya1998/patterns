2e29cbc8ab497dc272d291a2be7509ef0507a211
Evgesha <Slav.novikova2014@gmail.com>
2023-08-24 13:12:34 +0700

    changed test

--- src/test/java/ru/netology/patterns/test/CollbackTest.java
+++ src/test/java/ru/netology/patterns/test/CollbackTest.java
@@ -48,7 +48,7 @@ public class CollbackTest {
          //       .shouldBe(visible);
         //$("[data-test-id='replan-notification'] button").click();
         //$("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
+              //  .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
         //        .shouldBe(visible);
 
     }
